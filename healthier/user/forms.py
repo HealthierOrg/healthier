c5507cd6c94b4e5cc22870d19bc38f8b32cb7eeb
Olamilekan <lwahab@terragonltd.com>
2017-11-07 11:15:56 +0100

    Fixed Registration

--- healthier/user/forms.py
+++ healthier/user/forms.py
@@ -5,7 +5,6 @@ from healthier.user.models import HealthierUser
 
 
 class SignupForm(forms.ModelForm):
-
     class Meta:
         model = HealthierUser
         fields = ('account_type',)
@@ -15,13 +14,7 @@ class SignupForm(forms.ModelForm):
         user.email = self.cleaned_data['email']
         # Save the default User fields first, so you can get the User instance
         user.save()
-        healthier_user = HealthierUser.objects.get(request=user.email)
-        print(healthier_user.id)
-        print(type(healthier_user))
-        account_details = Provider(healthier_id=healthier_user) if user.account_type\
+        healthier_user = HealthierUser.objects.get(email=user.email)
+        account_details = Provider(healthier_id=healthier_user) if user.account_type \
                                                                    == "PRO" else Consumer(healthier_id=healthier_user)
         account_details.save()
-
-
-
-
