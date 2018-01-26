84a9c7ae60cfb5f83ec8f3c3808e0a39f1a62e6e
Olamilekan <lwahab@terragonltd.com>
2018-01-26 08:40:04 +0100

    Updating Postgre COnfig

--- migration_handler.sh
+++ migration_handler.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Starting ..."
-
-echo ">> Deleting old migrations"
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
-
 echo ">> Running manage.py makemigrations"
-python manage.py makemigrations
+python3.5 manage.py makemigrations
 
 echo ">> Running manage.py migrate"
-python manage.py migrate
+python3.5 manage.py migrate
 
 echo ">> Done"
 
