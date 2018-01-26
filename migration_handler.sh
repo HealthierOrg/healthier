b0d86c04ae6893c0a44e16c192fc68e2afa13767
Olamilekan <lwahab@terragonltd.com>
2018-01-26 07:15:57 +0100

    Updated Migration Handler

--- migration_handler.sh
+++ migration_handler.sh
@@ -1,34 +1,21 @@
-##!/usr/bin/env bash
-#
-#echo "Starting ..."
-#
-#echo ">> Deleting old migrations"
-#find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-#find . -path "*/migrations/*.pyc"  -delete
-#
-#
-## Optional
-#echo ">> Deleting database"
-#find . -name "db.sqlite3" -delete
-#
-#echo ">> Running manage.py makemigrations"
-#python manage.py makemigrations
-#
-#echo ">> Running manage.py migrate"
-#python manage.py migrate
-#
-#echo ">> Done"
-
-
 #!/usr/bin/env bash
 
-set -e
+echo "Starting ..."
 
-sourceApp="$1"
-targetApp="$2"
+echo ">> Deleting old migrations"
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
 
-while read key value; do
-  key=${key%%:}
-  echo "Setting $key=$value"
-  heroku config:set "$key=$value" --app "$targetApp"
-done  < <(heroku config --app "$sourceApp" | sed -e '1d')
+
+# Optional
+echo ">> Deleting database"
+find . -name "db.sqlite3" -delete
+
+echo ">> Running manage.py makemigrations"
+python manage.py makemigrations
+
+echo ">> Running manage.py migrate"
+python manage.py migrate
+
+echo ">> Done"
+
