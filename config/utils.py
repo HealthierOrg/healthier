45c690390512b59e78bffa453654fc369be3df56
Olamilekan <lwahab@terragonltd.com>
2018-02-01 08:17:30 +0100

    Debugging Static Assets

--- config/utils.py
+++ config/utils.py
@@ -7,7 +7,7 @@ from django.conf import settings
 def generate_id(account_type):
     chars = string.ascii_uppercase + string.digits
     account_map = "{0}_".format(account_type)
-    rand = ''.join(random.choice(chars) for _ in range(settings.ACCOUNT_ID_LENGTH)).lower()
+    rand = ''.join(random.choice(chars) for _ in range(20)).lower()
     return account_map + rand
 
 
