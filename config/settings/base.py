061ed5b8e66a1ba80974517fe10abc86cd5b1617
Olamilekan <lwahab@terragonltd.com>
2018-02-01 08:34:45 +0100

    Debugging Static Assets

--- config/settings/base.py
+++ config/settings/base.py
@@ -95,6 +95,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 # MIGRATIONS CONFIGURATION
